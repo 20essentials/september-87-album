979a4f6d3eda217a893e465494142c24dfedb75b
DominicDev <s0l0estudi4r@gmail.com>
2026-04-03 20:31:17 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'SEPTEMBER 87';
+export let nameSongs = [
+	"MAN EATER", /*1*/
+	"BAD DREAM BABY", /*2*/
+	"MEET ME AT THE DANCE", /*3*/
+	"RIDE ALL NIGHT", /*4*/
+	"EXILE", /*5*/
+	"THREE TRIBES", /*6*/
+	"ROOM SERVICE", /*7*/
+	"SURVEILLANCE", /*8*/
+	"SENTENCED TO SPACE", /*9*/
+	"ESCAPE TO THE RETRO FUTURE", /*10*/
+	"JOURNEY", /*11*/
+	"VICE CITY", /*12*/
+	"LOVE & VICE", /*13*/
+	"NEW BEGINNING", /*14*/
+	"CITY LIGHTS", /*15*/
+	"ON THE RUN", /*16*/
+	"THROUGH THE NIGHT", /*17*/
+	"MOON OVER LA", /*18*/
+	"LAST SUNSET", /*19*/
+	"REMEMBER WHEN", /*20*/
+]
